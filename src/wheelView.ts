0dce9f1040c2d6a93af188b68c47759b6abcde69
Anastasiia <32597582+HedFan@users.noreply.github.com>
2023-07-07 21:07:35 +0300

    TASK-006: add wheel tongue animation (#5)

--- src/wheelView.ts
+++ src/wheelView.ts
@@ -9,17 +9,19 @@ import {
     BREAKPOINTS,
     LAST_BREAKPOINT,
     WHEEL_POSITION,
-    WHEEL_PIVOT, SPEED
+    WHEEL_PIVOT, SPEED, TONGUE_BREAKPOINT
 } from './configs';
 
 export class WheelView extends PIXI.Container {
     private readonly _sendWinValueSubject$ = new Subject<{ winValue: number }>();
+    private readonly _sendTongueCollisionSubject$ = new Subject<void>();
     private readonly _app: PIXI.Application;
     private readonly _wheel: PIXI.Sprite;
     private _isSpinning = false;
-    private _winContainers = new Array<any>();
+    private _winContainers = new Array<PIXI.Text>();
     private _stopPosition: number | undefined;
     private _spinTimer: number | undefined;
+    private _checkingTongueBreakpoint: number[];
 
     constructor(app: PIXI.Application) {
         super();
@@ -32,6 +34,7 @@ export class WheelView extends PIXI.Container {
         for (let i = 0; i < WIN_VALUES.length; i++) {
             this._buildWinIndicator(i);
         }
+        this._checkingTongueBreakpoint = TONGUE_BREAKPOINT;
     }
 
     public spinWheel(winValue?: number): void {
@@ -47,16 +50,21 @@ export class WheelView extends PIXI.Container {
             return;
         }
         this._wheel.rotation += SPEED * delta;
-        if (this._wheel.rotation >= this._stopPosition && (this._stopPosition - this._wheel.rotation + 0.1) >= 0) {
+        const rotation = this._wheel.rotation;
+        this._findTongueBreakpoint(rotation);
+        if (rotation >= this._stopPosition && (this._stopPosition - rotation + 0.1) >= 0) {
             this._stopReels();
         }
-        if (this._wheel.rotation >= LAST_BREAKPOINT) {
+        if (rotation >= LAST_BREAKPOINT) {
             this._wheel.rotation = 0;
         }
     }
 
     get sendWinValue$(): Observable<{ winValue: number }> {
         return this._sendWinValueSubject$;
+    }
+    get sendTongueCollision$(): Observable<void> {
+        return this._sendTongueCollisionSubject$;
     }
 
     private _preparedToStop(winValue?: number): void {
@@ -93,10 +101,28 @@ export class WheelView extends PIXI.Container {
         this._wheel.addChild(winText);
     }
 
-    private _findClosesWinValue(currentValue: number) {
+    private _findClosesWinValue(currentValue: number): number {
         if (currentValue > BREAKPOINTS[BREAKPOINTS.length - 1]) {
             return BREAKPOINTS[0];
         }
         return BREAKPOINTS.filter(value => value > currentValue).sort((a, b) => a - b)[0];
+    }
+
+    private _findTongueBreakpoint(currentValue: number): void {
+        if (!this._checkingTongueBreakpoint.length) {
+            this._checkingTongueBreakpoint = TONGUE_BREAKPOINT;
+        }
+        if (currentValue > this._checkingTongueBreakpoint[5]) {
+          return;
+        }
+        let checkingValue: number;
+        const breakpointLength = this._checkingTongueBreakpoint.length;
+        for (let i = 0; i < breakpointLength; i++) {
+            checkingValue = this._checkingTongueBreakpoint[i];
+            if (currentValue >= checkingValue) {
+                this._sendTongueCollisionSubject$.next();
+                this._checkingTongueBreakpoint = this._checkingTongueBreakpoint.slice(i + 1);
+            }
+        }
     }
 }
